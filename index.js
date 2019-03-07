b8b6e3316279ea59bd5a2f0e65b49fc1a2f9f1b1
Henry  / Ryan <fireballs246@gmail.com>
2019-03-06 21:03:44 -0500

    Add database

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  //.set('views', path.join(__dirname, 'views'))
+  //.set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
