3194fa2b766b0fedb9f9562911fbddd0002754ef
maxh65000 <booah8@gmail.com>
2019-03-07 23:03:37 -0500

    Add /csv.csv

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
+const fetch = require('node-fetch')
+const json2csv = require('json2csv')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const apiKey = process.env.APIKEY
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,12 +13,38 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+	.get('/csv.csv', (req, res) => {
+		console.log("hi")
+		// get our data
+		fetch("https://strategy-e354.restdb.io/rest/strategy", {
+			headers: {
+		    "Content-Type": "application/json",
+		    "X-Apikey": apiKey
+		  }
+		}).
+		// make it json
+		then(x=>{
+			if(!x.ok) throw x
+			return x.json()
+		}).
+		// make it csv
+		then(data => {
+			res.setHeader('Content-Type', 'text/csv')
+			res.write(json2csv.parse(data))
+			res.end()
+		}).
+		catch(resp => {
+			console.error(resp);
+			res.error();
+		})
+	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 showTimes = () => {
   let result = ''
   const times = process.env.TIMES || 5
   for (i = 0; i < times; i++) {
     result += i + ' '
   }
-  return result;
+  return result
 }
